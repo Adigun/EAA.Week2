3e418728d95358f306b024f372bfc822a7c6a32b
Adigun <toheeb@hotmail.com>
2014-10-03 11:34:15 +0100

    data.sh

--- EAA.lab/week2/scripts/data.sh
+++ EAA.lab/week2/scripts/data.sh
@@ -5,4 +5,4 @@ echo "Username : $1"
 echo "Month of Birth : $2"
 echo "Favorite color : $3"
 
-printf "Hello "$1" you were born in "$2" and your favorite color is "$3"
+printf "Hello $1 you were born in $2 and your favorite color is $3"
