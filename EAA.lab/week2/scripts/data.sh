34a97eb95ee41c00d33b0c6a8d009a0e3ee04398
Adigun <toheeb@hotmail.com>
2014-10-03 11:28:01 +0100

    data.sh

--- EAA.lab/week2/scripts/data.sh
+++ EAA.lab/week2/scripts/data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Type the phone number:"
-read phone number
+#: Description: print Hello and the first command-line argument 
+echo "Username : $1"
+echo "Month of Birth : $2"
+echo "Favorite color : $3"
 
-echo "$phonenumber"
+printf "Hello %s!\n" "$1" you were born in %s!\n" "$2" and your favorite color is %s!\n" "$3"
