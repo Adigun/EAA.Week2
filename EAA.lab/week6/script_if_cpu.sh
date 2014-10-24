c60d40d1b886504d21f69e53063ed8cf88b315f9
Adigun <toheeb@hotmail.com>
2014-10-24 11:55:51 +0100

    logfiles

--- EAA.lab/week6/script_if_cpu.sh
+++ EAA.lab/week6/script_if_cpu.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
+
 if [ "$1" == "2" ]; then
 
 echo "Number of Processor:" 
 cat /proc/cpuinfo | grep processor | wc -l
 
 else
-    echo "You have type incorrect information"
+    echo "You have type incorrect Processor Number" >>logfilel
 fi
   
 
