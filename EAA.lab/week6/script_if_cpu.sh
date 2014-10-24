37161361c1a0104ea76301095c56e828b1ad4871
Adigun <toheeb@hotmail.com>
2014-10-24 11:33:41 +0100

    week6

---
+++ EAA.lab/week6/script_if_cpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+if [ "$1" == "2" ]; then
+
+echo "Number of Processor:" 
+cat /proc/cpuinfo | grep processor | wc -l
+
+else
+    echo "You have type incorrect information"
+fi
+  
+
